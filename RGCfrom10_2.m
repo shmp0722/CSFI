8115bdeee2505c38840720fab40f040c6291ca57
Shumpei Ogawa <shmp0722@gmail.com>
2017-05-14 04:53:27 +0900

    Spacial suming fited between 30-2 and 10-2

--- RGCfrom10_2.m
+++ RGCfrom10_2.m
@@ -1,24 +1,70 @@
 function RGCfrom10_2
 %% read data
+cd '/Users/shumpei/Google Drive/CSFI/CSFI';
+T = readtable('10-2forMatlab2.xlsx');
 
-T = readtable('10-2forMatlab.xlsx');
+%% remove subjects HFA reliability is not enough
+rows =  T.FP< .15 & T.FN<.33 & T.FixLoss_pcnt<.2;
 
-% %%
-% figure; hold on;
-% plot(T.RGC_OCT,T.MD10_2,'ob')
-% plot(T.RGC_HFA_Adjusted,T.MD10_2,'or')
-% plot(T.wRGC_adjusted, T.MD10_2,'og')
-% plot(T.wRGC, T.MD10_2,'ob')
+T2 = T(rows,:);
 
 %% plot 1
 figure; hold on;
-plot(T.MD10_2,T.RGC_OCT,'ob')
-plot(T.MD10_2,T.RGC_HFA_Adjusted,'or')
-plot( T.MD10_2,T.wRGC_adjusted,'og')
+plot(T2.MD10_2,T2.RGC_OCT,'ob')
+plot(T2.MD10_2,T2.RGC_HFA_Adjusted,'or')
+plot(T2.MD10_2,T2.wRGC_adjusted,'og')
 xlabel('MD value')
 ylabel('estimated num of RGC')
 legend({'OCT','HFA','weighted'})
 
-%%
-figure
-plot(T.RGC_OCT,T.RGC_HFA_Adjusted,'o')
+
+%% Bland Altman From matlab exchange
+
+addpath(genpath('BlandAltman'))
+label ={'RGC HFA','RGC OCT'};
+[rpc, fig, stats] = BlandAltman(T2.RGC_OCT,T2.RGC_HFA_Adjusted,label)
+
+%% lowess Y= MD
+figure; hold on;
+span = 0.5;
+[xx, inds] = sort(T2.RGC_HFA_Adjusted);
+yy1 = smooth(T2.RGC_HFA_Adjusted,T2.MD10_2,span,'rloess');
+plot(xx,yy1(inds),'r.');
+clear xx inds
+
+[xx, inds] = sort(T2.RGC_OCT);
+yy2 = smooth(T2.RGC_OCT,T2.MD10_2,span,'rloess');
+plot(xx,yy2(inds),'b.')
+clear xx inds
+
+[xx, inds] = sort(T2.wRGC);
+yy3 = smooth(T2.wRGC_adjusted,T2.MD10_2,span,'rloess');
+plot(xx,yy3(inds),'g.')
+clear xx inds
+
+xlabel 'RGC count'
+ylabel MD
+legend({'RGC HFA','RGC OCT','wRGC'})
+
+%% lowess X= MD
+figure; hold on;
+span = 0.5;
+[xx, inds] = sort(T2.MD10_2);
+yy1 = smooth(T2.MD10_2, T2.RGC_HFA_Adjusted, span,'rloess');
+plot(xx,yy1(inds),'r.');
+% clear xx inds
+
+% [xx, inds] = sort(T2.RGC_OCT);
+yy2 = smooth(T2.MD10_2,T2.RGC_OCT,span,'rloess');
+plot(xx,yy2(inds),'b.')
+% clear xx inds
+
+% [xx, inds] = sort(T2.wRGC_adjusted);
+yy3 = smooth(T2.MD10_2,T2.wRGC_adjusted,span,'rloess');
+plot(xx,yy3(inds),'g.')
+clear xx inds
+
+xlabel 'RGC count'
+ylabel MD
+legend({'RGC HFA','RGC OCT','wRGC'})
+
